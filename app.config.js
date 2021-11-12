f2c16bc961339b963ba08dd1ab3198fe75f4bb2f
Luca Milanese <luca.milanese90@gmail.com>
2021-11-12 09:53:58 +0100

    fix cookie banner background color

--- app.config.js
+++ app.config.js
@@ -74,5 +74,5 @@ module.exports = {
   // is used to create and show a privacy preference center / cookie banner
   // To learn more about how to configure and use this, please refer to the readme
   privacyPreferenceCenter:
-    process.env.GATSBY_PRIVACY_PREFERENCE_CENTER || 'false'
+    process.env.GATSBY_PRIVACY_PREFERENCE_CENTER || 'true'
 }
