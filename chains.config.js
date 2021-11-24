23f4d6da1101d650790746fd9ce94e086be276bb
Luca Milanese <luca.milanese90@gmail.com>
2021-11-24 17:20:41 +0100

    fix chainId config overwrite

--- chains.config.js
+++ chains.config.js
@@ -2,10 +2,9 @@
 // see: https://github.com/oceanprotocol/ocean.js/blob/e07a7cb6ecea12b39ed96f994b4abe37806799a1/src/utils/ConfigHelper.ts#L8
 // networkId is required, since its used to look for overwrites
 // all other fields are first loaded from ocean.js and are optional
-// import { ConfigHelperConfig } from '@oceanprotocol/lib'
-
 const chains = [
   {
+    name: 'Gaia-X',
     networkId: 2021000,
     nodeUri: 'https://rpc.gaiaxtestnet.oceanprotocol.com/',
     providerUri: 'https://provider.gaiax.delta-dao.com/',
