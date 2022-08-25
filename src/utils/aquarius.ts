87920b3fb6a132753a2abd776b37f63221de580b
Luca Milanese <luca.milanese90@gmail.com>
2022-08-25 12:10:01 +0200

    fix: aquarius id queries

--- src/utils/aquarius.ts
+++ src/utils/aquarius.ts
@@ -229,7 +229,7 @@ export async function getAssetsFromDidList(
         sortBy: SortTermOptions.Created,
         sortDirection: SortDirectionOptions.Descending
       },
-      filters: [getFilterTerm('id', didList)],
+      filters: [getFilterTerm('_id', didList)],
       ignorePurgatory: true
     } as BaseQueryParams
     const query = generateBaseQuery(baseParams)
@@ -255,7 +255,7 @@ export async function retrieveDDOListByDIDs(
         sortBy: SortTermOptions.Created,
         sortDirection: SortDirectionOptions.Descending
       },
-      filters: [getFilterTerm('id', didList)],
+      filters: [getFilterTerm('_id', didList)],
       ignorePurgatory: true
     } as BaseQueryParams
     const query = generateBaseQuery(baseQueryparams)
@@ -413,7 +413,7 @@ export async function getDownloadAssets(
         sortDirection: SortDirectionOptions.Descending
       },
       filters: [
-        getFilterTerm('id', didList),
+        getFilterTerm('_id', didList),
         getFilterTerm('service.type', 'access')
       ]
     } as BaseQueryParams
